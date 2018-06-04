c8a57bc0478c500aa0be341b721f5ef6772c09c2
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-06-04 13:00:46 +0200

    Main activity interacts with odometer service

--- app/src/main/java/es/montanus/odometer/MainActivity.java
+++ app/src/main/java/es/montanus/odometer/MainActivity.java
@@ -6,7 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends Activity {
 
@@ -32,6 +36,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayDistance();
+    }
+
+    private void displayDistance() {
+        final TextView distanceView = findViewById(R.id.distance);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.0;
+                if (bound && odometer != null) {
+                    distance = odometer.getDistance();
+                }
+                String distanceStr = String.format(Locale.getDefault(),
+                        "%1$,.2f miles", distance);
+                distanceView.setText(distanceStr);
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 
     @Override
