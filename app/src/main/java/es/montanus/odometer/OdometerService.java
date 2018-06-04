b568a01eb3323a332b9551d4a493d7547bf6f804
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-06-04 11:15:32 +0200

    Odometer service returns a random distance

--- app/src/main/java/es/montanus/odometer/OdometerService.java
+++ app/src/main/java/es/montanus/odometer/OdometerService.java
@@ -2,15 +2,33 @@ package es.montanus.odometer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
+import java.util.Random;
+
 public class OdometerService extends Service {
+
+    private final IBinder binder;
+    private final Random random;
+
     public OdometerService() {
+        this.binder = new OdometerBinder();
+        this.random = new Random();
+    }
+
+    private class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
+    }
+
+    public double getDistance() {
+        return random.nextDouble();
     }
 }
